c28d88e7e94d4208449472b5113098e0fa9b72a4
dbond <dbond@DESKTOP-SPNMLK3>
2024-02-20 01:01:57 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private const int ORDERS_QUANTITY = 10;
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;             
+        }
+        public Task<Order> GetOrder()
+        {
+            return _context.Orders
+                .OrderByDescending(order => order.Quantity * order.Price)
+                .FirstAsync();            
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _context.Orders
+                .Where(order => order.Quantity > ORDERS_QUANTITY)
+                .ToListAsync();            
+        }
+    }
+}
